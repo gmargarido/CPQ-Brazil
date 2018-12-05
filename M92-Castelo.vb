4cec6b494e827ff103b952b08149c9e6b6b9c72c
Glauco Elias Margarido <glauco.margarido@gmail.com>
2018-12-05 17:13:31 -0200

    Update M92-Castelo.vb

--- M92-Castelo.vb
+++ M92-Castelo.vb
@@ -1,7 +1,10 @@
 Sub Castelo(item_partnumber as String)
 	Dim var as String
 	
-	If CheckType(item_partnumber) = "Butterfly Valve" Then
+	If CheckType(item_partnumber) = "Knife Valve"
+		'Es necesario utilizar la query abajo en la tabla SUPORTE_GUILHOTINA - Celdas IH1:IL18
+		var = Select "Suporte Acionamento" from SUPORTE_GUILHOTINA where Trim Code = SuporteAcionamento(item_partnumber)
+	ElseIf CheckType(item_partnumber) = "Butterfly Valve" Then
 		var = select "Norma das Conexões" from /* MATERIAL_BORBOLETA */ where Modelo = Serie(item_partnumber)
 	ElseIf CheckType(item_partnumber) = "Ball Valve"
 		var = "Definir" 'Dejar el campo con esta mensaje como alerta para que el usuario deba escribir o definir
@@ -10,3 +13,11 @@ Sub Castelo(item_partnumber as String)
 	End If
 	Castelo = var
 End Sub
+
+Public Function SuporteAcionamento(item_partnumber as String)
+	Dim trim as String
+	
+	trim = Mid(item_partnumber,23,3)
+
+	SuporteAcionamento = trim
+End Function
