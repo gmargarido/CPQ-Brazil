26c8ddb57b7f168a91bee2465b316d1ca2ca1edb
Glauco Elias Margarido <glauco.margarido@gmail.com>
2018-12-05 16:07:03 -0200

    M93-Parafusos.vb

---
+++ M93-Parafusos.vb
@@ -0,0 +1,21 @@
+Sub Parafusos(item_partnumber as String)
+	Dim var as String
+	
+	If CheckType(item_partnumber) = "Knife Valve" Then
+		var = "N/A"
+	ElseIf CheckType(item_partnumber) = "Butterfly Valve" Then
+		var = "N/A"
+	ElseIf Serie(item_partnumber) = "19"
+		var = "N/A"
+	ElseIf Serie(item_partnumber) = "1A"
+		'Es necesario utilizar la query abajo en la tabla CORPO_1A - Celdas CT1:CW25
+		var = Select "Parafuso" from /* CORPO_1A */ where Corpo = Corpo(item_partnumber)
+	ElseIf Model(item_partnumber) = "S8000"
+		var = "INOX"
+	Else
+		'Es necesario utilizar la query abajo en la tabla CORPO - Celdas R1:U13
+		var = Select "Corpo" from /* CORPO */ where Cód = Corpo(item_partnumber)
+	EndIf
+	
+	Parafusos = var
+End Sub
