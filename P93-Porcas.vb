f3cb54ae495df7c3fd646cb04f89b30f190faf4a
Glauco Elias Margarido <glauco.margarido@gmail.com>
2018-12-05 16:12:21 -0200

    P93-Porcas.vb

--- P93-Porcas.vb
+++ P93-Porcas.vb
@@ -9,12 +9,12 @@ Sub Parafusos(item_partnumber as String)
 		var = "N/A"
 	ElseIf Serie(item_partnumber) = "1A"
 		'Es necesario utilizar la query abajo en la tabla CORPO_1A - Celdas CT1:CW25
-		var = Select "Parafuso" from /* CORPO_1A */ where Corpo = Corpo(item_partnumber)
+		var = Select "Porca" from /* CORPO_1A */ where Corpo = Corpo(item_partnumber)
 	ElseIf Model(item_partnumber) = "S8000"
 		var = "INOX"
 	Else
 		'Es necesario utilizar la query abajo en la tabla CORPO - Celdas R1:U13
-		var = Select "Parafuso" from /* CORPO */ where Cód = Corpo(item_partnumber)
+		var = Select "Porca" from /* CORPO */ where Cód = Corpo(item_partnumber)
 	EndIf
 	
 	Parafusos = var
