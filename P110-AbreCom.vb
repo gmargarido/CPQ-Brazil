93921ebc255b58b52facbfcf4ffeb1cb3c0d4a01
Glauco Elias Margarido <glauco.margarido@gmail.com>
2018-12-07 18:25:12 -0200

    AbreCom / FechaCom

---
+++ P110-AbreCom.vb
@@ -0,0 +1,29 @@
+Sub AbreCom(item_partnumber as String,actuator_partnumber as String)
+	Dim var as String
+	Dim Atuador as String
+	
+	Atuador = TrimAtuador(item_partnumber, actuator_partnumber)
+	
+	If CheckType(item_partnumber) = "Ball Valve" Then
+		If FalhaAtuador(item_partnumber, actuator_partnumber) = "-" Then
+			var = "-"
+		ElseIf FalhaAtuador(item_partnumber, actuator_partnumber) = "ÚLTIMA POSIÇÃO" Or FalhaAtuador(item_partnumber, actuator_partnumber) = "FECHADA"
+			var = AlimentAtuador(item_partnumber,actuator_partnumber)
+		Else
+			var = "MOLA"
+		End If
+	ElseIf CheckType(item_partnumber) = "Butterfly Valve" Or  CheckType(item_partnumber) = "Knife Valve"
+		If FalhaAtuador(item_partnumber, actuator_partnumber) = "ÚLTIMA POSIÇÃO" Or  FalhaAtuador(item_partnumber, actuator_partnumber) = "FECHADA"
+			var = AlimentAtuador(item_partnumber,actuator_partnumber)
+		ElseIf FalhaAtuador(item_partnumber, actuator_partnumber) = "ABERTA"
+			var = "MOLA"
+		Else
+			var = "-"
+		End If
+	Else
+		var = ""
+	End If
+	
+	FechaCom = var
+
+End Sub
